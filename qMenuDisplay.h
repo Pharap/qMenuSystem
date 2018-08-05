f553a006226a8852fa3c63c011f132e92801db0e
Pharap <2933055+Pharap@users.noreply.github.com>
2018-08-05 10:21:07 +0100

    Add flash string overloads

--- qMenuDisplay.h
+++ qMenuDisplay.h
@@ -3,6 +3,9 @@
 #ifndef qMenuDisplay_h
 #define qMenuDisplay_h
 
+// Predeclare Arduino class
+const __FlashStringHelper;
+
 class qMenuDisplay
 {
   public:
@@ -11,9 +14,12 @@ class qMenuDisplay
     void Start();
     void Finish();
     void Title(const char text[]);
-    void Item(int index,const char text[]);
+    void Title(const __FlashStringHelper * flashString);
+    void Item(int index, const char text[]);
+    void Item(int index, const __FlashStringHelper * flashString);
     void Highlight(int index);
     void MessageBox(const char text[]);
+    void MessageBox(const __FlashStringHelper * flashString);
   private:
 //    char tempBuffer[32];
 };
